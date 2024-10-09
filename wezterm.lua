8d896200877dc0f1212723eafddb7468ae5286d5
Aarjeet-1 <arjeetjoshi175@gmail.com>
2024-10-09 11:39:47 +0545

    changes

--- wezterm.lua
+++ wezterm.lua
@@ -1,82 +1,10 @@
 local wezterm = require("wezterm")
-local act = wezterm.action
-local config = wezterm.config_builder()
+local config  = {}
 
 
 
---tab-title
-local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
+config.color_scheme = "Tokyo Night"
+config.window_decoratin = "RESIZE"
 
-local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
-
-
-function tab_title(tab_info)
-  local title = tab_info.tab_title
-  -- if the tab title is explicitly set, take that
-  if title and #title > 0 then
-    return title
-  end
-  -- Otherwise, use the title from the active pane
-  -- in that tab
-  return tab_info.active_pane.title
-end
-
-wezterm.on(
-  'format-tab-title',
-  function(tab, tabs, panes, config, hover, max_width)
-    local edge_background = '#0b0022'
-    local background = '#1b1032'
-    local foreground = '#808080'
-
-    if tab.is_active then
-      background = '#2b2042'
-      foreground = '#c0c0c0'
-    elseif hover then
-      background = '#3b3052'
-      foreground = '#909090'
-    end
-
-    local edge_foreground = background
-
-    local title = tab_title(tab)
-
-    -- ensure that the titles fit in the available space,
-    -- and that we have room for the edges.
-    title = wezterm.truncate_right(title, max_width - 2)
-
-    return {
-      { Background = { Color = edge_background } },
-      { Foreground = { Color = edge_foreground } },
-      { Text = SOLID_LEFT_ARROW },
-      { Background = { Color = background } },
-      { Foreground = { Color = foreground } },
-      { Text = title },
-      { Background = { Color = edge_background } },
-      { Foreground = { Color = edge_foreground } },
-      { Text = SOLID_RIGHT_ARROW },
-    }
-  end
-)
-
-function scheme_for_appearance(appearance)
-  if appearance:find 'Dark' then
-    return   'Builtin Solarized Dark'
-else
-    return 'Builtin Solarized Light'
-  end
-end
-
-wezterm.on('window-config-reloaded', function(window, pane)
-  local overrides = window:get_config_overrides() or {}
-  local appearance = window:get_appearance()
-  local scheme = scheme_for_appearance(appearance)
-  if overrides.color_scheme ~= scheme then
-    overrides.color_scheme = scheme
-    window:set_config_overrides(overrides)
-  end
-end)
-
-
---don't know what it is
 
 return config
