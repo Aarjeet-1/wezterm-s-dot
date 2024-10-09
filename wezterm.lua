951d06d7164ab7a077f9f879de55df148a906097
Aarjeet-1 <arjeetjoshi175@gmail.com>
2024-10-10 00:41:43 +0545

    addin some decor and tab bar conf

--- wezterm.lua
+++ wezterm.lua
@@ -1,10 +1,36 @@
 local wezterm = require("wezterm")
-local config  = {}
+--local act = wezterm.action
+local config = wezterm.config_builder()
 
 
 
-config.color_scheme = "Tokyo Night"
-config.window_decoratin = "RESIZE"
 
+config ={
+  automatically_reload_config = true,
+  enable_tab_bar = true,
+  color_scheme = "Tokyo Night",
+  --window_background_opacity = 0.3
+  window_decorations = "RESIZE",
+  
+  window_padding = {
+    left = 3,
+    right = 3, 
+    top = 0,
+    bottom = 0
+
+
+  },
+  scrollback_lines = 3000,
+
+
+  --tab bar
+  use_fancy_tab_bar = false,
+  status_update_interval = 1000
+  
+
+
+}
 
 return config
+
+
