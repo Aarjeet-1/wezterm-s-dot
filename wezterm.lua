39e57cb84a2de9873bcac935660ada66a3f178c0
Aarjeet-1 <arjeetjoshi175@gmail.com>
2025-07-19 16:01:55 +0545

    changed colorscheme

--- wezterm.lua
+++ wezterm.lua
@@ -1,36 +1,31 @@
 local wezterm = require("wezterm")
---local act = wezterm.action
 local config = wezterm.config_builder()
 
+config = {
+	automatically_reload_config = true,
+	enable_tab_bar = true,
+	color_scheme = "tokyonight",
+	--window_background_opacity = 0.3
+	window_decorations = "NONE",
+	hide_tab_bar_if_only_one_tab = false,
 
+	-- font = wezterm.font("Monospace"),
 
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+	scrollback_lines = 1000,
+	--tab bar
 
-config ={
-  automatically_reload_config = true,
-  enable_tab_bar = true,
-  color_scheme = "Tokyo Night",
-  --window_background_opacity = 0.3
-  window_decorations = "RESIZE",
-  
-  window_padding = {
-    left = 3,
-    right = 3, 
-    top = 0,
-    bottom = 0
-
-
-  },
-  scrollback_lines = 3000,
-
-
-  --tab bar
-  use_fancy_tab_bar = false,
-  status_update_interval = 1000
-  
-
-
+	use_fancy_tab_bar = false,
+	status_update_interval = 30000,
+	tab_max_width = 32,
+	--window_frame = {}
 }
 
 return config
 
-
+--
