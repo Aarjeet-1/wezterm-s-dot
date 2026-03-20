05a5b7964f35b405aee845e8c20c479821d739c3
Aarjeet-1 <arjeetjoshi175@gmail.com>
2026-03-21 01:12:23 +0545

    keybind to rename tabbar

--- wezterm.lua
+++ wezterm.lua
@@ -5,11 +5,9 @@ config = {
 	automatically_reload_config = true,
 	enable_tab_bar = true,
 	color_scheme = "tokyonight",
-	--window_background_opacity = 0.3
+	--window_background_opacity = 0.3,
 	window_decorations = "NONE",
 	hide_tab_bar_if_only_one_tab = false,
-
-
 	window_padding = {
 		left = 0,
 		right = 0,
@@ -23,6 +21,21 @@ config = {
 	status_update_interval = 30000,
 	tab_max_width = 32,
 	--window_frame = {}
+     
+    keys = {
+    {
+      key = 'R',
+      mods = 'CTRL|SHIFT',
+      action = wezterm.action.PromptInputLine {
+        description = 'Enter new name for tab',
+        action = wezterm.action_callback(function(window, _, line)
+          if line then
+            window:active_tab():set_title(line)
+          end
+        end),
+      },
+    },
+  },
 }
 
 return config
